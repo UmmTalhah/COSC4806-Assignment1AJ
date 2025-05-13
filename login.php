c64da516f48066f21bdff021d51a9ca0f2f8a67d
Ayan J <90760565+UmmTalhah@users.noreply.github.com>
2025-05-13 03:09:12 +0000

    added personalized login form with username and password inputs.

--- login.php
+++ login.php
@@ -5,9 +5,17 @@ session_start();
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Login</title>
+    <title>Ayan's Secure Login</title>
     </head>
   <body>
+    <h2>Welcome to Ayan's Portal</h2>
+    <form method="post" action= "login.php">
+      <label>Username:</label><br>
+      <input type="text" name="username"> required><br><br>
+      <label>Password:</label><br>
+      <input type="password" name="password" required><br><br>
+      <button type="submit"> Login </button>
+      </form>
     
   </body>
 </html>
